aca341db44e9ea690b94ddbf19ad98d407c34cf5
HamBoy1 <31289226+HamBoy1@users.noreply.github.com>
2018-02-25 16:38:37 -0600

    Add files via upload

--- ping.js
+++ ping.js
@@ -1,16 +1,23 @@
 const request = require('request');
 
-var status= new String('');
+var status= new String('!');
+var myVar = setInterval(function() {ping();}, 2000);
+var runs = 0;
 
-function ping(){
-  request('http://www.google.com', function (error, response) {
-    if(error){
-      status="down";
-    } else {
-      status="up";
-    }
-    console.log(status);
-  });
-}
+function ping() {
+    request('http://www.google.com', function (error, response) {
+      if(error){
+        status="down";
+      } else {
+        status="up";
+      }
+      console.log(status);
+      runs++;
+      if(runs>=5){
+        clearInterval(myVar);
+      }
+    });
+  }
+
 
 module.exports = {ping}
